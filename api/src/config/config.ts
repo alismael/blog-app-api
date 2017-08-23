47f27aedef3bc26d11f363f40177c1f971190473
aliismael <alilion22@ymal.com>
2017-08-23 23:57:30 +0200

    - [config] Add mysql config

--- api/src/config/config.ts
+++ api/src/config/config.ts
@@ -1,5 +1,6 @@
 export let config = {
     port: 3000,
     host: '0.0.0.0',
-    mongoDb: { url: 'mongodb://localhost:27017/blog-app' }
+    mongoDb: { url: 'mongodb://localhost:27017/blog-app' },
+    mysql: { database: 'blog', username: 'root', password: 'root' }
 }
