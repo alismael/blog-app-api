ffd09c8f5fbda30d6491073c998d1ad7cd56f7ee
aliismael <alilion22@ymal.com>
2017-08-24 00:00:47 +0200

    - [BlogRouter] Use async/await instead callbacks

--- api/src/modules/blog/routes/BlogRouter.ts
+++ api/src/modules/blog/routes/BlogRouter.ts
@@ -1,54 +1,45 @@
 import * as express from 'express'
 import { Blog } from '../models/Blog'
-import { ObjectId } from 'mongodb'
 
 export let blogRouter = express.Router();
 
 // Default get route
-blogRouter.get('/', (req, res, next) => {
+blogRouter.get('/', async (req, res, next) => {
     
     let blog = new Blog();
 
     // Get all categories
-    blog.getAllBlogs( function (response) {
-        
-        // Return categories to response
-        res.json( response );
+    let blogs = await blog.getAllBlogs();
 
-    });
+    // Return categories to response
+    res.json( blogs );
 });
 
 
 // Default post route
-blogRouter.post('/', (req, res, next) => {
+blogRouter.post('/', async (req, res, next) => {
     
     let blog = new Blog();
     
     blog.title = req.body.title;
     blog.description = req.body.description;
-    blog.user_id = req.body.user_id;
-
 
     // Add new blog
-    blog.add( function (response) {
-        
-        // Return categories to response
-        res.json( response );
+    let response = await blog.add();
 
-    });
+    // Return categories to response
+    res.json( response );
 });
 
 // Default delete route
-blogRouter.delete('/', (req, res, next) => {
+blogRouter.delete('/', async (req, res, next) => {
     
     let blog = new Blog();
-    blog._id = ObjectId(req.body.id);
+    blog.id = req.body.id;
     
-    // delete blog
-    blog.delete( function (response) {
-        
-        // Return categories to response
-        res.json( response );
-
-    });
+    // Add new blog
+    let response = await blog.delete();
+    
+    // Return categories to response
+    res.json( response );
 });
