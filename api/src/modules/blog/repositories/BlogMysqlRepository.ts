d5463ed792db3306a281ad272ed15cc60b712cbb
aliasmael <alilion22@ymail.com>
2017-08-24 00:01:37 +0200

    - [BlogMysqlRepository] New blog repository for mysql

---
+++ api/src/modules/blog/repositories/BlogMysqlRepository.ts
@@ -0,0 +1,36 @@
+import { IBlogRepository } from './IBlogRepository'
+import { Blog } from './../models/Blog'
+import { knex } from './../../knex/knex'
+
+
+export class BlogMysqlRepository implements IBlogRepository {
+    
+    // get all blogs
+    public async getAllBlogs () {
+        return await knex
+                    .select('*')
+                    .from('blog')
+                    .catch( function (err) {
+                        return "error"
+                    });
+    }
+
+    // Add new blog
+    public async add (blog: Blog) {
+        return await knex('blog')
+                    .insert( blog )
+                    .catch( function (err) {
+                        return "error"
+                    });
+    }
+
+    // Delete blog
+    public async delete (blog: Blog) {
+        return await knex('blog')
+                    .where('id', blog.id)
+                    .del()
+                    .catch( function (err) {
+                        return "error"
+                    });
+    }
+}
