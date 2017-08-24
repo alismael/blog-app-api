7a72fdb8c94ceecdf209d26a14de76432945f029
aliismael <alilion22@ymal.com>
2017-08-25 00:22:28 +0200

    - [FileRouter] Use async/await instead callbacks

--- api/src/modules/file/routes/FileRouter.ts
+++ api/src/modules/file/routes/FileRouter.ts
@@ -5,7 +5,7 @@ import { ObjectId } from 'mongodb'
 export let fileRouter = express.Router();
 
 // Default get route
-fileRouter.get('/all', (req, res, next) => {
+fileRouter.get('/all', async (req, res, next) => {
 
     let file = new File();
 
@@ -14,62 +14,51 @@ fileRouter.get('/all', (req, res, next) => {
     
 
     // Get all files
-    file.getFiles(objectId, objectModel, function (response) {
-        
-        // Return files to response
-        res.json( response );
+    let response = await file.getFiles(objectId, objectModel);
 
-    });
+    // Return files to response
+    res.json( response );
 });
 
 // Default get route
-fileRouter.get('/:id', (req, res, next) => {
+fileRouter.get('/:id', async (req, res, next) => {
 
     let file = new File();
 
-    let _id = req.params.id;
+    let id = req.params.id;
 
-    // Get all files
-    file.getFile(_id, function (response) {
-        
-        // Return files to response
-        res.json( response );
+    // Get file
+    let response = await file.getFile(id);
 
-    });
+    // Return files to response
+    res.json( response );
 });
 
 // Default post route
-fileRouter.post('/', (req, res, next) => {
+fileRouter.post('/', async (req, res, next) => {
     
     let file = new File();
     
     file.title = req.body.title;
-    file.objectId = req.body.object_id;
-    file.objectModel = req.body.object_model;
-    file.created_by = ObjectId("599b5382152cbb292f440615");
-    file.created_at = Date().toLocaleString();
-
+    file.object_id = req.body.object_id;
+    file.object_model = req.body.object_model;
 
     // Add new file
-    file.add( function (response) {
-        
-        // Return categories to response
-        res.json( response );
+    let response = await file.add();
 
-    });
+    // Return categories to response
+    res.json( response );
 });
 
 // Default delete route
-fileRouter.delete('/', (req, res, next) => {
+fileRouter.delete('/', async (req, res, next) => {
     
     let file = new File();
-    file._id = ObjectId(req.body.id);
+    file.id = req.body.id;
     
     // delete file
-    file.delete( function (response) {
-        
-        // Return categories to response
-        res.json( response );
+    let response = await file.delete();
 
-    });
+    // Return categories to response
+    res.json( response );
 });
