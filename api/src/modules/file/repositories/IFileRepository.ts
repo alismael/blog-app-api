13561dedc3868bf39994aa1aaac145d48dc08b7b
aliasmael <alilion22@ymail.com>
2017-08-25 00:21:38 +0200

    - [File] Use number instead of objectId

--- api/src/modules/file/repositories/IFileRepository.ts
+++ api/src/modules/file/repositories/IFileRepository.ts
@@ -1,9 +1,8 @@
 import { File } from './../models/File'
-import { ObjectId } from 'mongodb'
 
 export interface IFileRepository {
-    getFile (_id: ObjectId, callback);
-    getFiles (objectId: ObjectId, objectModel: string, callback);
-    add (file: File, callback);
-    delete (file: File, callback);
+    getFile (id: number);
+    getFiles (objectId: number, objectModel: string);
+    add (file: File);
+    delete (file: File);
 }
