3310c27616ad2fed9716372b11d304c282938eeb
aliasmael <alilion22@ymail.com>
2017-08-25 00:19:22 +0200

    - [File] New repository for mysql

---
+++ api/src/modules/file/repositories/FileMysqlRepository.ts
@@ -0,0 +1,49 @@
+import { IFileRepository } from './IFileRepository'
+import { File } from './../models/File'
+import { knex } from './../../knex/knex'
+
+
+export class FileMysqlRepository implements IFileRepository {
+    
+    // get file
+    public async getFile (id: number) {
+        return await knex
+                    .select('*')
+                    .from('file')
+                    .where('id', id)
+                    .catch( function (err) {
+                        return "error"
+                    });
+    }
+
+    // get all files
+    public async getFiles (objectId: number, objectModel: string) {
+        return await knex
+                    .select('*')
+                    .from('file')
+                    .where('object_id', objectId)
+                    .where('object_model', objectModel)
+                    .catch( function (err) {
+                        return "error"
+                    });
+    }
+
+    // Add new file
+    public async add (file: File) {
+        return await knex('file')
+                    .insert( file )
+                    .catch( function (err) {
+                        return "error"
+                    });
+    }
+
+    // Delete file
+    public async delete (file: File) {
+        return await knex('file')
+                    .where('id', file.id)
+                    .del()
+                    .catch( function (err) {
+                        return "error"
+                    });
+    }
+}
