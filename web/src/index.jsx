abe0f24b45e1333f28e9a7df5ccd9f0ac41b7544
aliismael <alilion22@ymail.com>
2017-08-29 00:27:45 +0200

    - [Index] Render login form

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import {render} from 'react-dom';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import LoginPage from './containers/LoginPage.jsx'
 
 class App extends React.Component {
   render () {
-    return <p> Hello React!</p>;
+    return (
+            <MuiThemeProvider muiTheme={getMuiTheme()}>
+                <LoginPage />
+            </MuiThemeProvider>
+            );
   }
 }
 render(<App/>, document.getElementById('app'));
