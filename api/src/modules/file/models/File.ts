34cc11725d6a25c37696f688e163889deb6007cc
aliismael <alilion22@ymail.com>
2017-09-01 22:54:09 +0200

    - [File] Extending Entity model with some refactoring

--- api/src/modules/file/models/File.ts
+++ api/src/modules/file/models/File.ts
@@ -1,44 +1,34 @@
-import { FileService } from './../services/FileService'
+import { Entity } from './../../entity/models/Entity'
+import { IFileRepository } from './../repositories/IFileRepository'
+import { FileMysqlRepository } from './../repositories/FileMysqlRepository'
 
 // File repository
-let _fileService: FileService = new FileService();
+let _fileRepository: IFileRepository = new FileMysqlRepository();
 
-export class File {
+export class File extends Entity {
     
     
     // Public attributes
     public id: number;
     public title: string;
-    public object_model: string;
     public object_id: number;
+    public object_model: string;
     public guid: string;    
     public created_by: number;
     public created_at: string;
     public updated_by: number;
     public updated_at: string;
 
-    // Get file by guid
-    public getFile(guid: string) {
-        return _fileService.getFile(guid);
+    public tableName() {
+        return "file";
     }
 
-    // Get files by object_model, object_id
-    public getFiles(objectId: number, objectModel: string) {
-        return _fileService.getFiles( objectId, objectModel)
-    }
-
-    // Add new file
-    public add() {
-        return _fileService.add(this);
-    }
-
-    // Delete file by guid
-    public delete() {
-        return _fileService.delete(this.guid);
+    public tableColumns() {
+        return ['id', 'title', 'object_id', 'object_model', 'guid', 'created_by', 'created_at', 'updated_by', 'updated_at'];
     }
 
     // Attach file with object
     public attach(objectId: number, objectModel: string, filesGuid: string[]) {
-        return _fileService.attach(objectId, objectModel, filesGuid);
+        return _fileRepository.attach(objectId, objectModel, filesGuid);
     }
 }
