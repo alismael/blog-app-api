f04a009b9819eef4f866b6a6fdcc10235d1819b1
aliismael <alilion22@ymail.com>
2017-09-01 21:41:00 +0200

    - [BlogRepository] No need  for these functions as Blog extends Entity

--- api/src/modules/blog/repositories/BlogMysqlRepository.ts
+++ api/src/modules/blog/repositories/BlogMysqlRepository.ts
@@ -6,34 +6,4 @@ var uuid = require('uuid')
 
 export class BlogMysqlRepository implements IBlogRepository {
     
-    // get all blogs
-    public async getBlog (guid) {
-        return await knex
-                    .select('*')
-                    .from('blog')
-                    .where('guid', guid)
-                    .catch( function (err) {
-                        return "error"
-                    });
-    }
-
-    // Add new blog
-    public async add (blog: Blog) {
-        blog.guid = uuid.v1();
-        return await knex('blog')
-                    .insert( blog )
-                    .catch( function (err) {
-                        return "error"
-                    });
-    }
-
-    // Delete blog
-    public async delete (guid: string) {
-        return await knex('blog')
-                    .where('guid', guid)
-                    .del()
-                    .catch( function (err) {
-                        return "error"
-                    });
-    }
 }
