382a844045afb5329710cf1453231f4c0687ce28
aliismael <alilion22@ymail.com>
2017-09-04 01:10:03 +0200

    - [BlogService] Filtering in model service not repository

--- api/src/modules/blog/services/BlogService.ts
+++ api/src/modules/blog/services/BlogService.ts
@@ -3,24 +3,37 @@ import { Blog } from './../models/Blog'
 
 export class BlogService {
     private blog = new Blog();
-    
-    // Get blogs
-    public find (condition?: any) {
-        return this.blog.find(condition);
+
+    // Get blog by guid
+    public async findAll() {
+        return await this.blog
+            .find();
+    }
+   
+    // Get blog by guid
+    public async findByGuid(guid: string) {
+        return await this.blog
+            .find()
+            .where('guid', guid);
     }
 
     // Add new blog
-    public insert (blog: Blog) {
-        return this.blog.insert(blog);
+    public async insert(blog: Blog) {
+        return await this.blog
+            .insert(blog);
     }
 
     // Update blog
-    public update (updates: any, condition: any) {
-        return this.blog.update(updates, condition);
+    public async update(guid: string, updates: any) {
+        return await this.blog
+            .update(updates)
+            .where('guid', guid);
     }
 
     // Delete blog
-    public delete (condition: any) {
-        return this.blog.delete(condition);
+    public async delete(guid: string) {
+        return await this.blog
+            .delete()
+            .where('guid', guid);
     }
 }
