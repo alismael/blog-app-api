dc1a8b5a79d315454d67a68e4b5e5ff1492e2997
aliismael <alilion22@ymail.com>
2017-09-04 01:07:19 +0200

    - [Entity] Refactoring move condition to model service

--- api/src/modules/entity/repositories/EntityMysqlRepository.ts
+++ api/src/modules/entity/repositories/EntityMysqlRepository.ts
@@ -1,71 +1,48 @@
 import { IEntityRepository } from './IEntityRepository'
 import { Entity } from './../models/Entity'
-import  knex  from './../../knex/knex'
+import knex from './../../knex/knex'
 
 export class EntityMysqlRepository implements IEntityRepository {
     private _table: string;
     private _columns: Array<any>;
 
-    constructor (entity: Entity) {
+    constructor(entity: Entity) {
         this._table = entity.tableName();
         this._columns = entity.tableColumns();
     }
-    
+
+    public wrapper() {
+        return knex
+            .select('*')
+            .from(this._table).clone();
+    }
+
     // get entity
-    public async find (condition?: any) {
-        return await knex
-                    .select('*')
-                    .from(this._table)
-                    .where(function() {
-                        if ( condition ) {
-                            let that = this;
-                            Object.keys(condition).forEach(function(key) {
-                                that.where(key, condition[key]);
-                            })
-                        }
-                    })
-                    .catch( function (err) {
-                        return "error" + err
-                    });
+    public find(columns?: string[]) {
+        return knex
+            .select('*')
+            .from(this._table).clone();
     }
 
     // Add new entity
-    public async insert (entity: any) {
-        console.log(entity)
+    public async insert(entity: any) {
         return await knex(this._table)
-                    .insert( entity )
-                    .catch( function (err) {
-                        return "error" + err
-                    });
+            .insert(entity)
+            .catch(function (err) {
+                return "error" + err
+            });
     }
 
-    // Add new entity
-    public async update (updates: any, condition: any) {
-        return await knex(this._table)
-                    .where(function(){
-                        let that = this;
-                        Object.keys(condition).forEach(function(key) {
-                            that.where(key, condition[key]);
-                        })
-                    })
-                    .update(updates)
-                    .catch( function (err) {
-                        return "error" + err
-                    });
+    // Update new entity
+    public update(updates: any) {
+        return knex(this._table)
+            .update(updates)
+            .clone();
     }
 
     // Delete entity
-    public async delete (condition: any) {
-        return await knex(this._table)
-                    .where(function(){
-                        let that = this;
-                        Object.keys(condition).forEach(function(key) {
-                            that.where(key, condition[key]);
-                        })
-                    })
-                    .del()
-                    .catch( function (err) {
-                        return "error"
-                    });
+    public delete() {
+        return knex(this._table)
+            .del();
     }
 }
--- api/src/modules/entity/repositories/IEntityRepository.ts
+++ api/src/modules/entity/repositories/IEntityRepository.ts
@@ -1,8 +1,8 @@
 import { Entity } from './../models/Entity'
 
 export interface IEntityRepository {
-    find (condition?: any);
-    insert (entity: any);
-    update (updates: any, condition: any);
-    delete (condition: any);
+    find(columns?: string[]);
+    insert(entity: any);
+    update(updates: any);
+    delete();
 }
