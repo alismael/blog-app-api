fad92751240421439328b5df9d1d369d23ec5a4c
aliismael <alilion22@ymail.com>
2017-09-04 01:07:19 +0200

    - [Entity] Refactoring move condition to model service

--- api/src/modules/entity/models/Entity.ts
+++ api/src/modules/entity/models/Entity.ts
@@ -4,27 +4,27 @@ import { EntityMysqlRepository } from './../repositories/EntityMysqlRepository'
 export abstract class Entity {
     private _entityRepository: IEntityRepository = new EntityMysqlRepository(this);
 
-    abstract tableName() : string;
-    abstract tableColumns() : Array<any>;
+    abstract tableName(): string;
+    abstract tableColumns(): Array<any>;
 
-    public find(condition?: any) {
-        return this._entityRepository.find(condition);
+    public find(columns?: string[]) {
+        return this._entityRepository.find(columns);
     }
 
     public insert(entity: Entity) {
-        return this._entityRepository.insert( this.getDto(entity) );
+        return this._entityRepository.insert(this.getDto(entity));
     }
 
-    public update (updates: any, condition: any) {
-        return this._entityRepository.update( updates, condition );
+    public update(updates: any) {
+        return this._entityRepository.update(updates);
     }
 
-    public delete(condition) {
-        return this._entityRepository.delete(condition);
+    public delete() {
+        return this._entityRepository.delete();
     }
 
     private getDto(entity: Entity) {
-        let obj = { };
+        let obj = {};
         this.tableColumns().forEach(element => {
             obj[element] = entity[element];
         });
