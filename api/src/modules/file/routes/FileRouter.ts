24d276cf38b093c3cf51a0bccb9b2724faf82e10
aliismael <alilion22@ymail.com>
2017-09-04 01:49:26 +0200

    - [File] Refactoring to match Entity changes

--- api/src/modules/file/routes/FileRouter.ts
+++ api/src/modules/file/routes/FileRouter.ts
@@ -11,27 +11,30 @@ let fileService = new FileService();
 
 // Get files by object_model, object_id
 fileRouter.get('/all/:model/:id', async (req, res, next) => {
-    let files = await fileService.find( { 'object_id': req.params.id, 'object_model': req.params.model } );
-    res.json( files );
+    let objectId = req.params.id;
+    let objectModel = req.params.model;
+    let files = await fileService.findByObject(objectId, objectModel);
+    res.json(files);
 });
 
 // Get file with guid
 fileRouter.get('/:guid', async (req, res, next) => {
-    let file = await fileService.find( { 'guid': req.params.guid } );
-    res.json( file );
+    let guid = req.params.guid;
+    let file = await fileService.find(guid);
+    res.json(file);
 });
 
-fileRouter.post('/upload', async function(req, res){
+fileRouter.post('/upload', async function (req, res) {
 
     let fileObj = new File();
-    
+
     fileObj.guid = uuid.v1();
     fileObj.object_id = 1;
     fileObj.object_model = 'blog';
     fileObj.created_by = 1;
     fileObj.updated_by = 1;
 
-    var storage =   multer.diskStorage({
+    var storage = multer.diskStorage({
         destination: function (req, file, callback) {
             let uploadDir = './uploads/';
             let dest = uploadDir + fileObj.guid;
@@ -50,45 +53,45 @@ fileRouter.post('/upload', async function(req, res){
             }
             if (stat && !stat.isDirectory()) {
                 res.json("error");
-            } 
+            }
             callback(null, dest);
         },
         filename: function (req, file, callback) {
-            fileObj.title = file.originalname;    
+            fileObj.title = file.originalname;
             callback(null, file.originalname);
         }
     });
-    var upload = multer({ storage : storage}).single('file');
+    var upload = multer({ storage: storage }).single('file');
 
-    upload(req, res, async function(err) {
-        if(err) {
+    upload(req, res, async function (err) {
+        if (err) {
             res.json("error");
         }
-        
+
         // Add new file
         let response = await fileService.insert(fileObj);
-        res.json( response );
+        res.json(response);
     });
-});
-
-// Update file
-fileRouter.put('/:guid', async (req, res, next) => {
-    
-    let guid = req.params.guid;
-    let updates = req.body;
-
-    let response = await fileService.update(updates, {'guid': guid});
-    res.json( response );
-});
-
-// Default delete route
-fileRouter.delete('/:guid', async (req, res, next) => {
-    let response = await fileService.delete( {'guid': req.params.guid} );
-    res.json( response );
 });
 
 // Attach files with object_id, object_model
 fileRouter.put('/attach', async (req, res, next) => {
     let response = await fileService.attach(req.body.object_id, req.body.object_model, req.body.files);
-    res.json( response );
+    res.json(response);
+});
+
+// Update file
+fileRouter.put('/:guid', async (req, res, next) => {
+    let guid = req.params.guid;
+    let updates = req.body;
+
+    let response = await fileService.update(guid, updates);
+    res.json(response);
+});
+
+// Default delete route
+fileRouter.delete('/:guid', async (req, res, next) => {
+    let guid = req.params.guid;
+    let response = await fileService.delete(guid);
+    res.json(response);
 });
--- api/src/modules/file/services/FileService.ts
+++ api/src/modules/file/services/FileService.ts
@@ -3,29 +3,59 @@ import { File } from './../models/File'
 
 export class FileService {
     private file = new File();
-    
-    // Get files
-    public find (condition?: any) {
-        return this.file.find(condition);
+
+
+    // abstraction for finding file
+    public find(guid?: string) {
+        if (guid)
+            return this.findByGuid(guid);
+        else
+            return this.findAll();
+    }
+
+    // Get all files
+    public async findAll() {
+        return await this.file
+            .find();
+    }
+
+    // Get file by guid
+    public async findByGuid(guid: string) {
+        return await this.file
+            .find()
+            .where('guid', guid);
+    }
+
+    // Get file by guid
+    public async findByObject(objectId: number, objectModel: string) {
+        return await this.file
+            .find()
+            .where('object_id', objectId)
+            .where('object_model', objectModel);
     }
 
     // Add new file
-    public insert (file: File) {
-        return this.file.insert(file);
-    }
-
-    // Delete file
-    public delete (condition: any) {
-        return this.file.delete(condition);
+    public async insert(file: File) {
+        return await this.file
+            .insert(file);
     }
 
     // Update file
-    public update (updates: any, condition: any) {
-        return this.file.update(updates, condition);
+    public async update(guid: string, updates: any) {
+        return await this.file
+            .update(updates)
+            .where('guid', guid);
+    }
+
+    // Delete file
+    public async delete(guid: string) {
+        return await this.file
+            .delete()
+            .where('guid', guid);
     }
 
     // Attach file
-    public attach (object_id: number, object_model: string, fileGuid: string[]) {
+    public attach(object_id: number, object_model: string, fileGuid: string[]) {
         return this.file.attach(object_id, object_model, fileGuid);
     }
 }
