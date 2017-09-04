1c9608180198b8e54b941cca6a456d9f9b29c58f
aliismael <alilion22@ymail.com>
2017-09-04 02:12:19 +0200

    - [File] Change parameter name

--- api/src/modules/file/repositories/FileMysqlRepository.ts
+++ api/src/modules/file/repositories/FileMysqlRepository.ts
@@ -6,9 +6,9 @@ var uuid = require('uuid')
 export class FileMysqlRepository implements IFileRepository {
 
     // Attach file with object
-    public async attach (object_id: number, object_model: string, guid: string[]) {
+    public async attach (object_id: number, object_model: string, files: string[]) {
         return await knex('file')
-                    .where('guid', 'IN', guid)
+                    .where('guid', 'IN', files)
                     .update({
                         object_model: object_model,
                         object_id: object_id
