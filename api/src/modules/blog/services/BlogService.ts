10c2475735fe4f9d685805d28e48ca1bd37fc9a8
aliismael <alilion22@ymail.com>
2017-09-04 02:12:41 +0200

    - [Blog] Catch errors

--- api/src/modules/blog/services/BlogService.ts
+++ api/src/modules/blog/services/BlogService.ts
@@ -7,33 +7,48 @@ export class BlogService {
     // Get blog by guid
     public async findAll() {
         return await this.blog
-            .find();
+            .find()
+            .catch(function(err){
+                return err
+            });
     }
    
     // Get blog by guid
     public async findByGuid(guid: string) {
         return await this.blog
             .find()
-            .where('guid', guid);
+            .where('guid', guid)
+            .catch(function(err){
+                return err
+            });
     }
 
     // Add new blog
     public async insert(blog: Blog) {
         return await this.blog
-            .insert(blog);
+            .insert(blog)
+            .catch(function(err){
+                return err
+            });
     }
 
     // Update blog
     public async update(guid: string, updates: any) {
         return await this.blog
             .update(updates)
-            .where('guid', guid);
+            .where('guid', guid)
+            .catch(function(err){
+                return err
+            });
     }
 
     // Delete blog
     public async delete(guid: string) {
         return await this.blog
             .delete()
-            .where('guid', guid);
+            .where('guid', guid)
+            .catch(function(err){
+                return err
+            });
     }
 }
