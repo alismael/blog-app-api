472571df58a4a6d5a20e89f6964c324700b34990
aliismael <alilion22@ymail.com>
2017-09-08 19:24:26 +0200

    serve react from node server

--- api/src/webRoutes.ts
+++ api/src/webRoutes.ts
@@ -2,9 +2,10 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var express = require('express');
 var webpack = require('webpack');
-var config = require('./webpack.config.dev.js');
-var webRoutes = express();
+var config = require('./../../web/webpack.config.js');
 var compiler = webpack(config);
+
+export let webRoutes = express.Router();
 
 webRoutes.use(bodyParser.urlencoded({ extended: false }));
 webRoutes.use(bodyParser.json());
@@ -13,7 +14,7 @@ webRoutes.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath
 }));
 webRoutes.use(require('webpack-hot-middleware')(compiler));
-webRoutes.use('/public', express.static('./../../web/public'));
+webRoutes.use("/public", express.static(path.join(__dirname, './../../web/public')));
 webRoutes.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + "./../../web/index.html"));
 });
