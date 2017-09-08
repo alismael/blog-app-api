2d3485c58d94717c22d8948652625c38d66c53a0
aliismael <alilion22@ymail.com>
2017-09-08 20:53:56 +0200

    use mateerial-ui

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import routes from './routes';
 
 ReactDom.render(
-  <Router history={browserHistory} routes={routes} />,
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <Router history={browserHistory} routes={routes} />
+  </MuiThemeProvider>,
   document.querySelector('#app')
 );
