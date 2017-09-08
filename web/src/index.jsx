3f704bb722222407152b4b2d3e69ae8ac2e692ab
aliismael <alilion22@ymail.com>
2017-09-08 20:11:59 +0200

    React routes

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { browserHistory, Router } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
-ReactDom.render((
-  <MuiThemeProvider muiTheme={getMuiTheme()}>
-    <Router history={browserHistory} routes={routes} />
-  </MuiThemeProvider>), document.getElementById('app'));
+ReactDom.render(
+  <Router history={browserHistory} routes={routes} />,
+  document.querySelector('#app')
+);
