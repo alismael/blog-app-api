a7acb655c9a32ac4b6b3b34d62b242183559528a
aliismael <alilion22@ymail.com>
2017-09-08 18:05:14 +0200

    Seperate api and web routes

---
+++ api/src/webRoutes.ts
@@ -0,0 +1,19 @@
+var path = require('path');
+var bodyParser = require('body-parser');
+var express = require('express');
+var webpack = require('webpack');
+var config = require('./webpack.config.dev.js');
+var webRoutes = express();
+var compiler = webpack(config);
+
+webRoutes.use(bodyParser.urlencoded({ extended: false }));
+webRoutes.use(bodyParser.json());
+webRoutes.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+webRoutes.use(require('webpack-hot-middleware')(compiler));
+webRoutes.use('/public', express.static('./../../web/public'));
+webRoutes.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + "./../../web/index.html"));
+});
