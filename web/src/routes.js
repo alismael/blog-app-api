785fe83df47c991013711a10975bd0727abdad75
aliismael <alilion22@ymail.com>
2017-09-15 00:17:54 +0200

    dashboard routes

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import App from './components/App';
-import Home from './components/Home';
-import LoginPage from './containers/LoginPage';
+import App from './app/components/App';
+import Home from './app/components/Home';
+import LoginPage from './login/containers/LoginPage';
+import DashboardPage from './dashboard/containers/DashboardPage';
+
 export default (
   <Route path='/' component={App}>
     <IndexRoute component={Home} />
     <Route path='login' component={LoginPage} />
+    <Route path='dashboard' component={DashboardPage} />
     <Route path='*' component={Home} />
   </Route>
 );
