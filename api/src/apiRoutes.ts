6310315ffe71ddfb0661968373cacadfd98bac1f
aliismael <alilion22@ymail.com>
2017-09-16 22:42:00 +0200

    use cors for allow cross origin resource sharing

--- api/src/apiRoutes.ts
+++ api/src/apiRoutes.ts
@@ -1,11 +1,15 @@
 import * as express from 'express'
+import * as cors from 'cors'
 
 // Import modules api routes
-import { blogRouter } from './modules/blog/routes/BlogRouter';
-import { fileRouter } from './modules/file/routes/FileRouter';
+import { blogRouter } from './modules/blog/routes/BlogRouter'
+import { fileRouter } from './modules/file/routes/FileRouter'
 import { userRouter } from "./modules/user/routes/UserRouter"
 
 export let apiRoutes = express.Router();
+
+// use cors to allow host
+apiRoutes.use(cors())
 
 // use blog routes under /api/blog
 apiRoutes.use('/blog', blogRouter);
