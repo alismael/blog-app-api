b60067c1afedc0612e9763b80c3d0fdf7a35df6d
AbdelRahmanSalah <abdosalahh@gmail.com>
2017-09-16 10:05:22 +0200

    indent and remove webpack from nodejs

--- api/src/modules/knex/knex.ts
+++ api/src/modules/knex/knex.ts
@@ -1,12 +1,12 @@
-import  * as knex  from 'knex';
+import * as knex from 'knex';
 import { config } from './../../config/config';
 
 export default knex({
-    dialect: 'mysql',
-    connection: {
-        host: config.host,
-        user: config.mysql.username,
-        password: config.mysql.password,
-        database : 'blog'
-    }
+  dialect: 'mysql',
+  connection: {
+    host: config.host,
+    user: config.mysql.username,
+    password: config.mysql.password,
+    database: 'blog'
+  }
 });
