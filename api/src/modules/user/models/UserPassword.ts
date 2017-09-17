c08070e838b912ded6a87a13a76ea8c7580a044f
AbdelRahmanSalah <abdosalahh@gmail.com>
2017-09-17 07:31:06 +0200

    fat entity

--- api/src/modules/user/models/UserPassword.ts
+++ api/src/modules/user/models/UserPassword.ts
@@ -1,60 +1,55 @@
+import { UserId } from "./User";
+import { Trace } from "./../../common/models";
 import { Entity } from "../../entity/models/Entity";
 
-export type RegistrationRequest = {
+export interface IRegistrationRequest {
   username: string
   email: string
   password: string
   repeatedPassword: string
 }
 
-export class UserPassword extends Entity {
+export class UserPasswordData {
+  constructor(public username: string, public email: string, public password: string) {}
+}
 
-  // public
-  public user_id: number
-  public password: string
-  public email: string
-  public username: string
-  public created_by: number;
-  public created_at: Date;
-  public updated_by: number;
-  public updated_at: Date;
+export class UserPassword {
 
-  public constructor(username: string, email: string, password: string) {
-    super()
-    this.password = password
-    this.email = email
-    this.username = username
-  }
-
-  tableName(): string {
-    return "user_password"
-  }
-  tableColumns(): string[] {
-    return ["email", "username", "password", "user_id", "created_by", "updated_by", "created_at", "updated_at"]
-  }
-
-  static vaidateRegistrationRequest = (registrationRequest: RegistrationRequest): Promise<UserPassword> => {
+  public static vaidateRegistrationRequest = (registrationRequest: IRegistrationRequest): Promise<UserPasswordData> => {
     return new Promise((resolve, reject) => {
       if (registrationRequest.email && registrationRequest.password && registrationRequest.username) {
-        let userPassword = new UserPassword(registrationRequest.username, registrationRequest.email, registrationRequest.password)
+        const userPassword = new UserPasswordData(
+          registrationRequest.username,
+          registrationRequest.email,
+          registrationRequest.password
+        )
         if (UserPassword.validateUserPasswordDataModel(userPassword)
           && (registrationRequest.password === registrationRequest.repeatedPassword))
           resolve(userPassword)
         else
           reject("Invalid request")
-      }
-      else
+      }else
         reject("Invalid request")
     })
   }
 
-  static validateUserPasswordDataModel = (userPassword: UserPassword): boolean => {
-    let email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  public static validateUserPasswordDataModel = (userPassword: UserPasswordData): boolean => {
+    const email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     return email.test(userPassword.email)
   }
+
+  public constructor(
+    public userId: UserId,
+    public data: UserPasswordData,
+    public trace: Trace
+  ) {}
 }
 
-
-
-
-
+export class UserPasswordEntity extends Entity {
+  public tableName(): string {
+    return "user_password"
+  }
+  public tableColumns(): string[] {
+    return ["email", "username", "password", "user_id", "created_by", "updated_by", "created_at", "updated_at"]
+  }
+}
