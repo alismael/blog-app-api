02f4757dce7d383173999c0cb83f40b49aff09f4
ali ismael <alilion22@ymail.com>
2017-09-30 20:51:21 +0200

    blog model incomplete

--- blog/api/src/config/config.ts
+++ blog/api/src/config/config.ts
@@ -1,8 +1,7 @@
 export let config = {
   port: 3000,
   host: '0.0.0.0',
-  mongoDb: { url: 'mongodb://localhost:27017/blog-app' },
-  mysql: { database: 'blog', username: 'root', password: 'abdo' },
+  mysql: { database: 'blog', username: 'root', password: 'blog' },
   hash: {
     saltRounds: 10
   }
