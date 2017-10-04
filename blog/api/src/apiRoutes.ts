126c7d765da760eb5a1c6dea85509739caf285bd
AbdelRahmanSalah <abdosalahh@gmail.com>
2017-10-04 07:22:08 +0200

    fix bug in userService

--- blog/api/src/apiRoutes.ts
+++ blog/api/src/apiRoutes.ts
@@ -4,7 +4,7 @@ import * as cors from 'cors'
 // Import modules api routes
 import { blogRouter } from './modules/blog/routes/BlogRouter';
 // import { fileRouter } from './modules/file/routes/FileRouter';
-import { userRouter } from "./modules/user/routes/UserRouter"
+import userRouter from "./modules/user/routes/UserRouter"
 
 export let apiRoutes = express.Router();
 
