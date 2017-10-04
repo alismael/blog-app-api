32a0ac6ae999a4c969f0be4074f1707caacb41e4
AbdelRahmanSalah <abdosalahh@gmail.com>
2017-10-04 07:26:10 +0200

    edit gulp file

--- blog/api/gulpfile.js
+++ blog/api/gulpfile.js
@@ -1,30 +1,7 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
-const tsfmt = require('gulp-tsfmt')
-const changedInPlace = require('gulp-changed-in-place')
 // pull in the project Typescript config
 const tsProject = ts.createProject('tsconfig.json');
-
-
-gulp.task('format', () => {
-  gulp.src('**/*.ts')
-    .pipe(changedInPlace())
-    .pipe(tsfmt({
-      IndentSize: 2,
-      TabSize: 2,
-      NewLineCharacter: "\n",
-      ConvertTabsToSpaces: true,
-      InsertSpaceAfterCommaDelimiter: true,
-      InsertSpaceAfterSemicolonInForStatements: true,
-      InsertSpaceBeforeAndAfterBinaryOperators: true,
-      InsertSpaceAfterKeywordsInControlFlowStatements: true,
-      InsertSpaceAfterFunctionKeywordForAnonymousFunctions: false,
-      InsertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis: false,
-      PlaceOpenBraceOnNewLineForFunctions: false,
-      PlaceOpenBraceOnNewLineForControlBlocks: false
-    }))
-    .pipe(gulp.dest(file => path.dirname(file.path)));
-});
 
 //task to be run when the watcher detects changes
 gulp.task('scripts', () => {
