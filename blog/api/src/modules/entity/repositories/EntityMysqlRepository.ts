93ee2858c72cf430fb2d436f7766454881ac6eab
ali ismael <alilion22@ymail.com>
2017-10-06 22:04:04 +0200

    map db record to it's equevilant model

--- blog/api/src/modules/entity/repositories/EntityMysqlRepository.ts
+++ blog/api/src/modules/entity/repositories/EntityMysqlRepository.ts
@@ -16,10 +16,12 @@ interface IOkPacket {
 }
 
 export class EntityMysqlRepository<T, S extends Primative> implements IEntityRepository<T, S> {
+  private _entity: Entity<T, S>;
   private _table: string;
   private _columns: Array<any>;
 
   constructor(entity: Entity<T, S>) {
+    this._entity = entity;
     this._table = entity.tableName();
     this._columns = entity.tableColumns();
   }
@@ -27,49 +29,58 @@ export class EntityMysqlRepository<T, S extends Primative> implements IEntityRep
   // get entity
   public find(columns?: string[]): DBIO<T[]> {
     let query = squel.select({ separator: "\n" })
-    .from(this._table)
-    .toParam()
+      .from(this._table)
+      .toParam()
 
     return new DBIO<T[]>(query.text, query.values)
+      .map(entities => {
+        return entities.map((entity) => {
+          return this._entity.map(entity)
+        })
+      })
   }
 
   public findOne(column: ColumnValue<T, S>): DBIO<Maybe<T>> {
     let query = squel.select({ separator: "\n" })
-    .from(this._table)
-    .where(`${column.columnName} = ?`, [column.value])
-    .toParam()
+      .from(this._table)
+      .where(`${column.columnName} = ?`, [column.value])
+      .toParam()
 
     return new DBIO<T[]>(query.text, query.values)
-    .map(users => {
-      return users.head()
-    })
+      .map(entites => {
+        console.log('entites', entites)
+        return entites.head().map((enitiy) => {
+          console.log(enitiy)
+          return this._entity.map(enitiy)
+        })
+      })
   }
 
   // Add new entity
   public insert(columns: ColumnValue<T, S>[]): DBIO<number> {
-    let cols = columns.reduce((acc, next) => 
-    Object.assign(acc, {[next.columnName]: `${next.value}`}) 
-   , {})
+    let cols = columns.reduce((acc, next) =>
+      Object.assign(acc, { [next.columnName]: `${next.value}` })
+      , {})
 
     let query = squel.insert()
-    .into(this._table)
-    .setFields(cols)
-    .toParam()
+      .into(this._table)
+      .setFields(cols)
+      .toParam()
 
     return new DBIO<IOkPacket>(query.text, query.values)
-    .map(result => result.insertId)
+      .map(result => result.insertId)
   }
 
   // Update new entity
   public update(columns: ColumnValue<T, S>[]): DBIO<number> {
-    let cols = columns.reduce((acc, next) => 
-    Object.assign(acc, {[next.columnName]: next.value}) 
-   , {})
+    let cols = columns.reduce((acc, next) =>
+      Object.assign(acc, { [next.columnName]: next.value })
+      , {})
 
     let query = squel.update()
-    .table(this._table)
-    .setFields(cols)
-    .toParam()
+      .table(this._table)
+      .setFields(cols)
+      .toParam()
 
     return new DBIO<number>(query.text, query.values)
   }
