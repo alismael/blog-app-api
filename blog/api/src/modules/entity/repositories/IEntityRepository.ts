a38ea5662c0c5051a18719f482b0037057d7898a
ali ismael <alilion22@ymail.com>
2017-10-07 19:56:59 +0200

    add condition paramater

--- blog/api/src/modules/entity/repositories/IEntityRepository.ts
+++ blog/api/src/modules/entity/repositories/IEntityRepository.ts
@@ -5,7 +5,7 @@ import { DBIO } from "../../../libs/IO";
 export interface IEntityRepository<T, S extends Primative> {
   find(columns?: string[]): DBIO<T[]>
   insert(columns: ColumnValue<T, S>[]): DBIO<number>
-  update(columns: ColumnValue<T, S>[]): DBIO<number>
+  update(condition: ColumnValue<T, S>, columns: ColumnValue<T, S>[]): DBIO<number>
   // delete()
   findOne(column: ColumnValue<T, S>): DBIO<Maybe<T>>
 }
