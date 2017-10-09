c8551dd51941097d89ff9598adc46b187110e030
ali ismael <alilion22@ymail.com>
2017-10-09 23:45:58 +0200

    Get user blogs

--- blog/api/src/modules/blog/routes/BlogRouter.ts
+++ blog/api/src/modules/blog/routes/BlogRouter.ts
@@ -3,23 +3,24 @@ import { Blog, BlogData } from '../models/Blog'
 import { BlogService } from '../services/BlogService'
 import { connection } from '../../mysql/mysql'
 import * as uuid from 'uuid'
+import { Id } from '../../common/models';
 
 export let blogRouter = express.Router();
 
 let blogService = new BlogService();
 
-// Get all blogs
+// Get current user blogs
 blogRouter.get('/', (req, res, next) => {
-
-  blogService.findAll()
-    .execute(connection)
-    .then(blogs => {
-      res.json(blogs)
-    })
-    .catch(err => {
-      res.sendStatus(500)
-      console.log(err)
-    })
+  let currentUserId: Id = 1 // Get blogs for user with id = 1
+  blogService.getUserBlogs(currentUserId)
+  .execute(connection)
+  .then(blogs => {
+    res.json(blogs)
+  })
+  .catch(err => {
+    res.sendStatus(500)
+    console.log(err)
+  })
 });
 
 // Get blog
@@ -59,17 +60,17 @@ blogRouter.post('/', (req, res, next) => {
 // Update blog
 blogRouter.put('/:guid', (req, res, next) => {
   BlogData.vaidateInsertBlogRequest(req.body)
-  .then(blogData => {
-    blogService.update(req.params.guid, blogData)
-      .execute(connection)
-      .then(_ => res.sendStatus(200))
-      .catch(err => {
-        res.sendStatus(500)
-        console.log(err)
-      })
-  })
-  .catch(err => {
-    res.sendStatus(500)
-    console.log(err)
-  })
+    .then(blogData => {
+      blogService.update(req.params.guid, blogData)
+        .execute(connection)
+        .then(_ => res.sendStatus(200))
+        .catch(err => {
+          res.sendStatus(500)
+          console.log(err)
+        })
+    })
+    .catch(err => {
+      res.sendStatus(500)
+      console.log(err)
+    })
 });
