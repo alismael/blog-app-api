1c640407b683e83a9dbe6c17af1acf73c6305dd7
ali ismael <alilion22@ymail.com>
2017-10-09 23:43:33 +0200

    change private members to protected, change find fmethod to protected

--- blog/api/src/modules/entity/repositories/IEntityRepository.ts
+++ blog/api/src/modules/entity/repositories/IEntityRepository.ts
@@ -3,8 +3,7 @@ import { Entity, ColumnValue, Primative } from './../models/Entity'
 import { DBIO } from "../../../libs/IO";
 
 export interface IEntityRepository<T, S extends Primative> {
-  find(columns?: string[]): DBIO<T[]>
+  findOne(column: ColumnValue<T, S>): DBIO<Maybe<T>>
   insert(columns: ColumnValue<T, S>[]): DBIO<number>
   update(condition: ColumnValue<T, S>, columns: ColumnValue<T, S>[]): DBIO<number>
-  findOne(column: ColumnValue<T, S>): DBIO<Maybe<T>>
 }
