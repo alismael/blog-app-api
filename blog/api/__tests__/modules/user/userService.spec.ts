277a17912b543c9be35f55a875aacdb567af05d7
AbdelRahmanSalah <abdosalahh@gmail.com>
2017-10-19 07:35:44 +0200

    add logger and register test case

--- blog/api/__tests__/modules/user/userService.spec.ts
+++ blog/api/__tests__/modules/user/userService.spec.ts
@@ -2,28 +2,17 @@ import { db } from './../../db';
 import { UserFactory } from './../factory';
 import { UserService } from './../../../src/modules/user/services/UserService';
 import { connection } from "../../../src/modules/mysql/mysql";
+let log = require('./../../../src/logger');
+
 describe("user service tests", () => {
   let service = new UserService
   let factory = new UserFactory
-  test("create user", () => {
+  
+  test("create user", (done) => {
     let action = service.register(factory.userPasswordData) 
     db.run(action, (r) => {
-      expect(r).toBe(1)
+      expect(r).toBeGreaterThan(0)
+      done()
     })
-    // connection.beginTransaction(err => {
-    //   if (err)
-    //     connection.rollback(() => {
-    //       return
-    //     })
-    //   else {
-    //     service.register(factory.userPasswordData).execute(connection, true)
-    //       .then(r => {
-    //         expect(r).toBe(1)
-    //         connection.rollback(() => {
-    //           return
-    //         })
-    //       })
-    //   }
-    // })
   })
 })
