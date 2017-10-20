b8b3afb94279012d9341d60f7229683ec1a5dea1
ali ismael <alilion22@ymail.com>
2017-10-20 08:02:52 +0200

    create blog test

--- blog/api/__tests__/modules/factory.ts
+++ blog/api/__tests__/modules/factory.ts
@@ -1,5 +1,10 @@
 import { UserPasswordData } from "../../src/modules/user/models/UserPassword";
+import { BlogData } from "../../src/modules/blog/models/Blog";
 
 export class UserFactory {
   public userPasswordData = new UserPasswordData("user1", "test@test.com", "123456")
+}
+
+export class BlogFactory {
+  public blogData = new BlogData("blog title", "blog description")
 }
