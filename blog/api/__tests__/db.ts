f75a81358c603f724cc7bed163bc35cf18638bde
ali ismael <alilion22@ymail.com>
2017-10-22 00:49:55 +0200

    Run IO but never commit transaction to DB

--- blog/api/__tests__/db.ts
+++ blog/api/__tests__/db.ts
@@ -1,21 +1,33 @@
-import { connection } from './../src/modules/mysql/mysql';
-import { DBIO } from './../src/libs/IO';
+import { connection } from './../src/modules/mysql/mysql'
+import { DBIO } from './../src/libs/IO'
 import global from "./../src/global"
-let logger = require('./../../api/src/logger');
+let logger = require('./../../api/src/logger')
 
 global()
 
 class DBContext {
-  public run<T>(dbIO: DBIO<T>, fn: (result: T) => void) {
-    let rollbackIO = dbIO.flatMap(result => {
-      fn(result)
-      return DBIO.failed("rollback")
-    })
-
-    DBIO.run(connection, rollbackIO)
-    .catch(err => {
-      logger.debug(err)
-      throw err
+  run<T>(ioAction: DBIO<T>) {
+    return new Promise<T>((resolve, reject) => {
+      connection.beginTransaction(err => {
+        if (err)
+          connection.rollback(() => {
+            reject(err)
+          })
+        else {
+          ioAction.execute(connection)
+            .then(a => {
+              resolve(a)
+              connection.rollback(() => {
+                reject()
+              })
+            })
+            .catch(catchErr => {
+              return connection.rollback(() => {
+                reject(catchErr)
+              })
+            })
+        }
+      })
     })
   }
 }
