ab723c298e513ecc9bd566df02414388923105b5
ali ismael <alilion22@ymail.com>
2017-10-22 00:51:30 +0200

    replace callback with promise

--- blog/api/__tests__/modules/user/userService.spec.ts
+++ blog/api/__tests__/modules/user/userService.spec.ts
@@ -1,18 +1,22 @@
-import { db } from './../../db';
-import { UserFactory } from './../factory';
-import { UserService } from './../../../src/modules/user/services/UserService';
-import { connection } from "../../../src/modules/mysql/mysql";
-let log = require('./../../../src/logger');
+import { db } from './../../db'
+import { UserFactory } from './../factory'
+import { UserService } from './../../../src/modules/user/services/UserService'
+import { connection } from "../../../src/modules/mysql/mysql"
+let logger = require('../../../src/logger')
 
 describe("user service tests", () => {
   let service = new UserService
   let factory = new UserFactory
-  
+
   test("create user", (done) => {
-    let action = service.register(factory.userPasswordData) 
-    db.run(action, (r) => {
-      expect(r).toBeGreaterThan(0)
-      done()
-    })
+    let action = service.register(factory.userPasswordData)
+    db.run(action)
+      .then(result => {
+        expect(result).toBeGreaterThan(0)
+        done()
+      })
+      .catch(err => {
+        throw err
+      })
   })
 })
