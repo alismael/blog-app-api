24c2a0f50b805c2e28407a20a5f363d232bfdaff
ali ismael <alilion22@ymail.com>
2017-10-27 23:27:36 +0200

    passing user to blog service

--- blog/api/src/modules/blog/services/BlogService.ts
+++ blog/api/src/modules/blog/services/BlogService.ts
@@ -2,7 +2,7 @@ import { Blog, blogEntity, BlogUUID, BlogId, BlogData } from "../models/Blog"
 import { DBIO } from "../../../libs/IO"
 import { Maybe } from 'tsmonad'
 import { Trace, Id } from './../../common/models'
-import { UserId, User } from "../../user/models/User"
+import { User } from "../../user/models/User"
 import * as uuid from "uuid"
 
 export class BlogService {
@@ -18,22 +18,20 @@ export class BlogService {
   }
 
   // Add new blog
-  insert(data: BlogData): DBIO<number> {
-    let userId = new UserId(1) // use test id=1
+  insert(data: BlogData, user: User): DBIO<number> {
     return blogEntity.insert(
       blogEntity.uuid.set(new BlogUUID(uuid.v4())),
       ...blogEntity.data.columns(new BlogData(data.title, data.description)),
-      ...blogEntity.trace.columns(Trace.createTrace(userId))
+      ...blogEntity.trace.columns(Trace.createTrace(user.id))
     )
   }
 
   // Update blog
-  update(guid: string, data: BlogData): DBIO<number> {
-    let userId = new UserId(2) // use test id=2
+  update(guid: string, data: BlogData, user: User): DBIO<number> {
     return blogEntity.update(
       blogEntity.uuid.set(new BlogUUID(guid)),
       ...blogEntity.data.columns(new BlogData(data.title, data.description)),
-      ...blogEntity.trace.updated.columns(Trace.createTrace(userId).updated)
+      ...blogEntity.trace.updated.columns(Trace.createTrace(user.id).updated)
     )
   }
 
