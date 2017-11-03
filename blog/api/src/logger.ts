053cacce32f032ceae3114feb2a719d6bc535fca
AbdelRahmanSalah <abdosalahh@gmail.com>
2017-11-03 09:35:57 +0200

    init route testing

--- blog/api/src/logger.ts
+++ blog/api/src/logger.ts
@@ -1,35 +1,13 @@
-import { Logger, transports } from "winston";
+import { createLogger, transports } from "winston";
 
-
-const customColors = {
-  trace: 'white',
-  debug: 'blue',
-  info: 'green',
-  warn: 'yellow',
-  crit: 'red',
-  error: 'red'
-};
-
-let config = {
-  colors: customColors,
-
-  levels: {
-    trace: 0,
-    debug: 1,
-    info: 2,
-    warn: 3,
-    crit: 4,
-    error: 5
-  },
+export const logger = createLogger({
   transports: [
     new transports.Console({
-      name: 'consoleLogger',
-      level: 'error',
-      colorize: true,
-      timestamp: true
+      level: "info",
+      handleExceptions: true,
+      json: false,
+      colorize: true
     })
-  ]
-}
-
-
-export const logger = new Logger(config);
+  ],
+  exitOnError: false
+});
--- blog/api/src/modules/user/routes/UserRouter.ts
+++ blog/api/src/modules/user/routes/UserRouter.ts
@@ -6,31 +6,40 @@ import { RegistrationError } from "./../models/UserPassword"
 import * as express from "express"
 import { DBIO } from "../../../libs/IO"
 
-const userRouter = express.Router()
-const userService = new UserService()
-
-userRouter.post("/register", (req, res) => {
-  UserPassword.vaidateRegistrationRequest(req.body)
+export class UserRouter {
+  public userService: UserService = new UserService
+  
+  register(req: express.Request, res: express.Response) {
+    UserPassword.vaidateRegistrationRequest(req.body)
     .then(userPassword => 
-      DBIO.run(connection, userService.register(userPassword))
+      DBIO.run(connection, this.userService.register(userPassword))
     )
     .then(_ => res.sendStatus(201))
     .catch((err: ErrorHandler<RegistrationError>) => {
-      err.apply(res)
+      if(err.apply)
+        err.apply(res)
+      else {
+        res.send(err)
+      }
     })
-})
+  }
 
-userRouter.post("/login", (req, res) => {
-  DBIO.run(connection, userService.login(req.body.username, req.body.password))
+  login(req: express.Request, res: express.Response) {
+    DBIO.run(connection, this.userService.login(req.body.username, req.body.password))
     .then(result => {
       res.cookie("token", result)
-      res.send({
+      res.status(200).send({
         token: result
       })
     })
     .catch((err: ErrorHandler<string>) => {
       err.apply(res)
     })
-})
-
-export default userRouter
+  }
+  route(): express.Router {
+    let userRoute = express.Router()
+    userRoute.post("/register", (req, res) => this.register(req, res))
+    userRoute.post("/login", (req, res) => this.login(req, res))
+    return userRoute
+  }
+}
