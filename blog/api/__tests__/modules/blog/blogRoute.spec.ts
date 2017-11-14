bb386aa072fac1d29d7b853b622b2e4d44c601cc
AbdelRahmanSalah <abdosalahh@gmail.com>
2017-11-14 22:51:01 +0200

    fix some comments in blog router

--- blog/api/__tests__/modules/blog/blogRoute.spec.ts
+++ blog/api/__tests__/modules/blog/blogRoute.spec.ts
@@ -1,72 +1,49 @@
 import { DBIO } from './../../../src/libs/IO';
 import { User } from './../../../src/modules/user/models/User';
-import { Id } from './../../../src/modules/common/models';
+import { Id, Trace } from './../../../src/modules/common/models';
 import { Blog, BlogData } from './../../../src/modules/blog/models/Blog';
 import { Maybe } from 'tsmonad';
 import { BlogRouter } from './../../../src/modules/blog/routes/BlogRouter';
-import { BlogFactory } from './../../factories/BlogFactory';
+import blogFactory from './../../factories/BlogFactory';
 import { UserRouter } from './../../../src/modules/user/routes/UserRouter';
 import { UserService } from "../../../src/modules/user/services/UserService";
 import { UserPasswordData } from "../../../src/modules/user/models/UserPassword";
 import * as supertest from "supertest"
 import * as express from "express"
 import * as bodyParser from "body-parser"
-import { JWT } from "../../../src/modules/user/models/User";
+import { JWT, UserUUID, UserId } from "../../../src/modules/user/models/User";
 import {} from "jest";
 import { UserFactory } from "../../factories/UserFactory";
 import { BlogService } from '../../../src/modules/blog/services/BlogService';
+import blogServiceMock from "./BlogServiceMock"
 
 describe("blog route tests", () => {
-  const factory = new BlogFactory
   const userFactory = new UserFactory
+  const userId: UserId = new UserId(1)
+  const userModel: User = new User(this.userId, new UserUUID("test"), userFactory.userData, Trace.createTrace(this.userId))
 
-  let blogRoute = new class extends BlogRouter {
-    blogService = new class extends BlogService {
-        // Get blog by guid
-  findByGuid(guid: string): DBIO<Maybe<Blog>> {
-    return DBIO.successful(Maybe.just(factory.blog))
-  }
-
-  // Get blog by guid
-  findById(id: Id): DBIO<Maybe<Blog>> {
-    return DBIO.successful(Maybe.just(factory.blog))
-  }
-
-  // Add new blog
-  insert(data: BlogData, user: User): DBIO<number> {
-    return DBIO.successful(1)
-  }
-
-  // Update blog
-  update(guid: string, data: BlogData, user: User): DBIO<number> {
-    return DBIO.successful(1)
-  }
-
-  // Get user blogs
-  getUserBlogs(user: User): DBIO<Blog[]> {
-    return DBIO.successful([factory.blog])
-  }
-    }()
+  const blogRoute = new class extends BlogRouter {
+    blogService: BlogService = blogServiceMock
   }()
 
   const app = express()
   app.use(bodyParser.json())
   // middleware mockups
   app.use((req, _, next) => {
-    req.body.user = DBIO.successful(Maybe.just(userFactory.userModel))
+    req.body.user = DBIO.successful(Maybe.just(userModel))
     next()
   })
   app.use(blogRoute.route())
 
   test("find blog by uuid", done => {
     supertest(app)
-      .get(`/${factory.blog.guid.value}`)
+      .get(`/${blogFactory.blog.guid.value}`)
       .set('Accept', "application/json")
       .expect('Content-Type', /json/)
       .expect(200)
       .end((err, res) => {
         if(err) throw err
-        expect(res.body).toMatchObject(factory.blog.toDto())
+        expect(res.body).toMatchObject(blogFactory.blog.toDto())
         done()
       })
   })
@@ -79,7 +56,7 @@ describe("blog route tests", () => {
       .expect(200)
       .end((err, res) => {
         if(err) throw err
-        expect(res.body).toMatchObject([factory.blog.toDto()])
+        expect(res.body).toMatchObject([blogFactory.blog.toDto()])
         done()
       })
   })
--- blog/api/src/modules/common/models.ts
+++ blog/api/src/modules/common/models.ts
@@ -9,15 +9,6 @@ export class Signture {
   constructor(public by: UserId, public at: Date) { }
 }
 
-export class TraceDto {
-  constructor(
-    public createdBy: Id,
-    public createdAt: Date,
-    public updatedBy: Id,
-    public updatedAt: Date
-  ) { }
-}
-
 export class Trace {
   public static createTrace(userId: UserId): Trace {
     return new Trace(new Signture(userId, new Date()), new Signture(userId, new Date()))
