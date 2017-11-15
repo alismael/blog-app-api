3250866a6572bc95d2fd3ed55749aeafcb260041
ali ismael <alilion22@ymail.com>
2017-11-16 00:42:20 +0200

    user login (not completed)

---
+++ blog/web/src/auth/reducers/authReducers.js
@@ -0,0 +1,26 @@
+export default function reducer(state={
+    user: {},
+    fetching: false,
+    fetched: false,
+    error: null,
+  }, action) {
+
+    switch (action.type) {
+      case "USER_LOGIN": {
+        return {...state, fetching: true}
+      }
+      case "USER_LOGIN_REJECTED": {
+        return {...state, fetching: false, error: action.payload}
+      }
+      case "USER_LOGIN_FULFILLED": {
+        return {
+          ...state,
+          fetching: false,
+          fetched: true,
+          user: action.payload,
+        }
+      }
+    }
+
+    return state
+}
--- blog/web/src/reducer.js
+++ blog/web/src/reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux"
 
 import blogs from "./dashboard/reducers/blogReducers"
+import user from "./auth/reducers/authReducers"
 
 export default combineReducers({
+  user,
   blogs
 })
