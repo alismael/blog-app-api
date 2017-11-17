8cb01fba42fd5ff65373354f5a1e69f036cd0787
ali ismael <alilion22@ymail.com>
2017-11-17 18:51:30 +0200

    Add form to support `enter` button

--- blog/web/src/auth/components/LoginForm.jsx
+++ blog/web/src/auth/components/LoginForm.jsx
@@ -4,49 +4,51 @@ import { Card, CardText, RaisedButton, TextField } from 'material-ui';
 
 
 const LoginForm = ({
-    onSubmit,
-    onChange,
-    errors,
-    user
+  onSubmit,
+	onChange,
+	errors,
+	user
 }) => (
-    <Card className="login-container">
-        <h2 className="card-heading">Login</h2>
+		<form onSubmit={onSubmit}>
+			<Card className="login-container">
+				<h2 className="card-heading">Login</h2>
 
-        <p className="error-message">{errors.summary}</p>
+				<p className="error-message">{errors.summary}</p>
 
-        <div className="field-line">
-            <TextField
-                floatingLabelText="Username"
-                name="username"
-                errorText={errors.username}
-                onChange={onChange}
-                value={user.username}
-            />
-        </div>
+				<div className="field-line">
+					<TextField
+						floatingLabelText="Username"
+						name="username"
+						errorText={errors.username}
+						onChange={onChange}
+						value={user.username}
+					/>
+				</div>
 
-        <div className="field-line">
-            <TextField
-                floatingLabelText="Password"
-                type="password"
-                name="password"
-                onChange={onChange}
-                errorText={errors.password}
-                value={user.password}
-            />
-        </div>
+				<div className="field-line">
+					<TextField
+						floatingLabelText="Password"
+						type="password"
+						name="password"
+						onChange={onChange}
+						errorText={errors.password}
+						value={user.password}
+					/>
+				</div>
 
-        <div className="button-line">
-            <RaisedButton type="submit" label="Log in" onClick={onSubmit} primary />
-        </div>
-    </Card>
-        
-    );
+				<div className="button-line">
+					<RaisedButton type="submit" label="Log in" primary />
+				</div>
+			</Card>
+		</form>
+
+	);
 
 LoginForm.propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-    onChange: PropTypes.func.isRequired,
-    errors: PropTypes.object.isRequired,
-    user: PropTypes.object.isRequired
+	onSubmit: PropTypes.func.isRequired,
+	onChange: PropTypes.func.isRequired,
+	errors: PropTypes.object.isRequired,
+	user: PropTypes.object.isRequired
 };
 
 export default LoginForm;
