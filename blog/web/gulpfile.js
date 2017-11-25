6b3702e9243fd0d35aef97fe4cfcb64704e3f9d0
aliasmael <alilion22@ymail.com>
2017-11-25 22:04:57 +0200

    Gulp file for watching typescript changes

---
+++ blog/web/gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+// pull in the project Typescript config
+const tsProject = ts.createProject('tsconfig.json');
+
+//task to be run when the watcher detects changes
+gulp.task('scripts', () => {
+  const tsResult = tsProject.src()
+    .pipe(tsProject());
+  return tsResult.js.pipe(gulp.dest('dest'));
+});
+
+//set up a watcher to watch over changes
+gulp.task('watch', ['scripts'], () => {
+  gulp.watch('./src/**/*.ts*', ['scripts']);
+});
+
+gulp.task('default', ['watch']);
