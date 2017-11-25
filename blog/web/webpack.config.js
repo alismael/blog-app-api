80f4f6515dfc6ca2171d51296c1408c0116ea53d
aliasmael <alilion22@ymail.com>
2017-11-25 22:05:26 +0200

    Change APP_DIR to dest folder

--- blog/web/webpack.config.js
+++ blog/web/webpack.config.js
@@ -2,10 +2,10 @@ var webpack = require('webpack');
 var path = require('path');
 
 var BUILD_DIR = path.resolve(__dirname, 'public/');
-var APP_DIR = path.resolve(__dirname, 'src/');
+var APP_DIR = path.resolve(__dirname, 'dest/');
 
 var config = {
-    entry: APP_DIR + '/index.jsx',
+    entry: APP_DIR + '/index.js',
     output: {
         path: BUILD_DIR,
         filename: 'bundle.js',
