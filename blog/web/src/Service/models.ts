b84499e5e1682e6403f5ec0500268e001993a87c
aliasmael <alilion22@ymail.com>
2017-11-30 18:23:55 +0200

    Add new models

--- blog/web/src/Service/models.ts
+++ blog/web/src/Service/models.ts
@@ -2,3 +2,30 @@ export interface ILoginRequest {
   username: string
   password: string
 }
+
+export interface Blog {
+  id: number,
+  guid: string
+  img: string,
+  data: {
+    title: string,
+    description: string
+  },
+  trace: {
+    createdBy: string,
+    createdAt: string,
+    updatedBy: string,
+    updatedAt: string
+  },
+}
+
+export interface IBlogState {
+  blogs: Blog[],
+  fetching: boolean,
+  fetched: boolean,
+  error: string,
+}
+
+export interface Store {
+  blogs: IBlogState
+}
