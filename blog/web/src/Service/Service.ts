c38771beb0e77ac2a0940bd3e50f7d6a07085002
aliasmael <alilion22@ymail.com>
2017-11-30 18:24:30 +0200

    Add new service for fetching new blogs

--- blog/web/src/Service/Service.ts
+++ blog/web/src/Service/Service.ts
@@ -4,9 +4,14 @@ import axios from "axios"
 
 class Service {
   private loginUrl = "/api/user/login"
+  private fetchBlogsUrl = "/api/blog"
 
   login(request: ILoginRequest) {
     return axios.post(this.loginUrl, request)
+  }
+
+  fetchBlogs() {
+    return axios.get(this.fetchBlogsUrl)
   }
 
 }
