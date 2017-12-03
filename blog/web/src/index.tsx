5af8d635b8891f9dcb0877299f0a6b9b7960bbd9
aliasmael <alilion22@ymail.com>
2017-12-03 20:34:08 +0200

    Add react routes

--- blog/web/src/index.tsx
+++ blog/web/src/index.tsx
@@ -1,18 +1,20 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom"
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import { Provider } from "react-redux"
-import store from "./store"
-
-import DashboardPage from "./dashboard/containers/DashboardPage"
+import { Provider } from 'react-redux'
+import store from './store'
+import { BrowserRouter } from 'react-router-dom'
+import { RouteMap } from './routes'
 
 const app = document.getElementById('app')
 
 ReactDOM.render(
   <Provider store={store}>
     <MuiThemeProvider muiTheme={getMuiTheme()}>
-      <DashboardPage />
+      <BrowserRouter>
+          <RouteMap />
+      </BrowserRouter>
     </MuiThemeProvider>
   </Provider>,
   app
