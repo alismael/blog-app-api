b16872a4ce661e933d56e24e95c45b5b73bebf72
aliasmael <alilion22@ymail.com>
2017-12-24 22:01:20 +0200

    VerticalDevider component

---
+++ blog/web/src/app/components/VerticalDivider.tsx
@@ -0,0 +1,38 @@
+import * as React from "react"
+
+const styles = {
+	divider: {
+		borderTopWidth: 0,
+		borderRightWidth: 3,
+		borderRightStyle: 'solid',
+		height: 45,
+		top: 0,
+    bottom: 0,
+    margin: 'auto',
+		borderRightColor: '#e1e1e1'
+	}
+};
+
+export interface IVerticalDividerProps {
+	
+}
+
+class VerticalDivider extends React.Component<IVerticalDividerProps> {
+	/**
+	 * Class constructor.
+	 */
+	constructor(props: IVerticalDividerProps) {
+		super(props);
+	}
+
+	// Render the component.
+	render() {
+
+		return (
+            <div style={styles.divider}/>
+		);
+	}
+
+}
+
+export default VerticalDivider;
