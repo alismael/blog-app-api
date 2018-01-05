c7d84f909ac3a3675601f2eadaa87377c21e65f2
aliasmael <alilion22@ymail.com>
2018-01-06 01:51:46 +0200

    Remove materiaal ui

--- blog/web/src/index.tsx
+++ blog/web/src/index.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import { Provider } from 'react-redux'
 import store from './store'
 import { BrowserRouter } from 'react-router-dom'
@@ -11,11 +9,9 @@ const app = document.getElementById('app')
 
 ReactDOM.render(
   <Provider store={store}>
-    <MuiThemeProvider muiTheme={getMuiTheme()}>
-      <BrowserRouter>
-          <RouteMap />
-      </BrowserRouter>
-    </MuiThemeProvider>
+    <BrowserRouter>
+      <RouteMap />
+    </BrowserRouter>
   </Provider>,
   app
 );
